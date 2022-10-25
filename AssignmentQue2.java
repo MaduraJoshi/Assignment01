f34331acc32177ca77f558728530a7a22e8a77c5
MaduraJoshi <115575946+MaduraJoshi@users.noreply.github.com>
2022-10-25 22:04:31 +0530

    Add files via upload

---
+++ AssignmentQue2.java
@@ -0,0 +1,26 @@
+
+public class AssignmentQue2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n =5;
+		
+		for(int i=1; i<n; i++) {
+			
+			//For printing I
+			for(int j=1; j<n; j++) {
+				
+				if(i==1 || i<=n-1)
+					System.out.print(i);
+					
+					else{
+						System.out.print(j);
+					}
+			}
+			System.out.println();
+
+	}
+
+}
+}
---
+++ INeuronPattern.java
@@ -0,0 +1,97 @@
+
+public class INeuronPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =11;
+		
+		for(int i=0; i<n; i++) {
+			
+			//For printing I
+			for(int j=0; j<n; j++) {
+				
+				if(i==0 || i==n-1 || j==(n-1)/2)
+					System.out.print("*");
+					
+					else{
+						System.out.print(" ");
+					}
+			}
+			System.out.print("  ");
+			// For printing N 
+			for(int j=0; j<n; j++) {
+				
+				if(j==0 && i<=n-1 || j==n-1 && i<=n-1 || i==j)
+					System.out.print("*");
+					
+					else{
+						System.out.print(" ");
+					}
+		} 
+			System.out.print("  ");
+			//For printing E 
+			for(int j=0; j<n; j++) {
+				
+				if(i==0 && i<=n-1 || j==0 && j<=(n-1)/2 || i==n-1 && j<=(n-1) || i==(n-1)/2 && j<=(n-1)/2)
+					System.out.print("*");
+					
+					else{
+						System.out.print(" ");
+					}
+		} 
+			System.out.print("  ");
+			// For printing U
+			for(int j=0; j<n; j++) {
+				
+				if(j==0 && j<=n-1 || i==n-1 && i<=n-1 || j==n-1 && i<=n-1 )
+					System.out.print("*");
+					
+					else{
+						System.out.print(" ");
+					}
+		} 
+		
+			System.out.print("  ");
+			// For printing R 
+			for(int j=0; j<n; j++) {
+				
+				if(j==0 || i==0 && j!=(n-1) || j==(n-1) && i<(n-1)/2 && i!=0 && i!=(n-1) || i==(n-1)/2 && j!=(n-1)|| i==j && i>=n/2 && j>=n/2)
+					System.out.print("*");
+					
+					else{
+						System.out.print(" ");
+					}
+		} 
+			System.out.print("  ");
+			
+			//For printing 0
+			for(int j=0; j<n; j++) {
+				
+				if(j==0 && i<=n-1 || i==n-1 && j<=n-1 || i==0 && j<=n-1 || j==n-1 && i<=n-1)
+					System.out.print("*");
+					
+					else{
+						System.out.print(" ");
+					}
+		}
+			System.out.print("  ");
+			
+			// For printing N 
+			for(int j=0; j<n; j++) {
+							
+				 if(j==0 && i<=n-1 || j==n-1 && i<=n-1 || i==j)
+					System.out.print("*");
+								
+					else{
+						System.out.print(" ");
+								}
+					} 
+			System.out.println();
+			
+
+	}
+		
+}
+}
+	
+		
